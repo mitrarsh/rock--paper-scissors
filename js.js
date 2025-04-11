e301a28129eec41659bfd7cdb2c7c7188fba4c17
mitrarsh <mitraroshanfekr05@gmail.com>
2025-04-12 02:09:31 +0330

    name

--- js.js
+++ js.js
@@ -4,9 +4,19 @@ var scissorsEl = document.getElementById("scissors");
 var choiceEls = document.querySelectorAll(".choice");
 var pcOptions = document.querySelectorAll(".pcoption");
 var resetbtnEl= document.getElementById("resetbtn");
+var nameEl = document.getElementById("name");
 
 var userScore = 0;
 var pcScore = 0;
+
+var userName = prompt("What's your name?");
+if(userName){
+alert("Welcome, " + userName + "!"); nameEl.textContent=userName}
+else{alert("No name? We'll just call you Player 😄"); nameEl.textContent="player"
+}
+
+
+
 
 pcOptions.forEach(option => option.style.opacity = "0");
 
@@ -31,8 +41,6 @@ var  compareChoices= function(pcChoice,userChoice){
 var updateScore= function(){
     document.getElementById("pcscore").textContent=pcScore;
     document.getElementById("userscore").textContent=userScore;
-
-
 };
 
 
@@ -48,14 +56,18 @@ var options = ["rock","paper","scissors"];
 const randomIndex= Math.floor(Math.random()* options.length);
 var pcChoice = options[randomIndex];
 
-if(options[randomIndex]==="rock"){document.getElementById("pcrock").style.opacity=1;}
-else if (options[randomIndex]==="paper"){document.getElementById("pcpaper").style.opacity=1;}
-else if (options[randomIndex]==="scissors"){document.getElementById("pcscissors").style.opacity=1;};
+if(options[randomIndex]==="rock"){document.getElementById("pcrock").style.opacity=1; 
+    document.getElementById("pcrock").classList.add("scale");}
+else if (options[randomIndex]==="paper"){document.getElementById("pcpaper").style.opacity=1; 
+    document.getElementById("pcpaper").classList.add("scale");}
+else if (options[randomIndex]==="scissors"){document.getElementById("pcscissors").style.opacity=1;
+    document.getElementById("pcscissors").classList.add("scale");};
+
 
 var result=compareChoices(pcChoice,userChoice);
 updateScore();
-// alert(result);}
-}
+ alert(result);}
+
 
 
 choiceEls.forEach(function(button){
