de500c82157986c720e7f83404e78cc79663460d
mitrarsh <mitraroshanfekr05@gmail.com>
2025-04-12 00:32:23 +0330

    scoring

--- js.js
+++ js.js
@@ -1,7 +1,73 @@
 var rockEl = document.getElementById("rock");
 var paperEl = document.getElementById("paper");
 var scissorsEl = document.getElementById("scissors");
+var choiceEls = document.querySelectorAll(".choice");
+var pcOptions = document.querySelectorAll(".pcoption");
+var resetbtnEl= document.getElementById("resetbtn");
+
+var userScore = 0;
+var pcScore = 0;
+
+pcOptions.forEach(option => option.style.opacity = "0");
+
+var nextRound = function(){    pcOptions.forEach(option=>option.style.opacity="0");
+    choiceEls.forEach(button=> button.classList.remove("select"));}
+;
+//scoring
+var  compareChoices= function(pcChoice,userChoice){
+    if (userChoice===pcChoice){return"it's a tie!"};
+
+    if(userChoice==="rock"&&pcChoice==="scissors"||
+        userChoice==="paper"&&pcChoice==="rock"||
+        userChoice==="scissors"&&pcChoice==="paper") {
+            userScore++;
+            return "You win!";
+        } else {
+            pcScore++;
+            return "PC wins!";
+        }
+    }
+
+var updateScore= function(){
+    document.getElementById("pcscore").textContent=pcScore;
+    document.getElementById("userscore").textContent=userScore;
+
+
+};
+
 
 var userChoose= function(event){
-    
+    nextRound();
+
+    var userChoice = event.target.id;
+event.target.classList.add("select");
+
+
+
+var options = ["rock","paper","scissors"];
+const randomIndex= Math.floor(Math.random()* options.length);
+var pcChoice = options[randomIndex];
+
+if(options[randomIndex]==="rock"){document.getElementById("pcrock").style.opacity=1;}
+else if (options[randomIndex]==="paper"){document.getElementById("pcpaper").style.opacity=1;}
+else if (options[randomIndex]==="scissors"){document.getElementById("pcscissors").style.opacity=1;};
+
+var result=compareChoices(pcChoice,userChoice);
+updateScore();
+// alert(result);}
 }
+
+
+choiceEls.forEach(function(button){
+    button.addEventListener("click", userChoose);
+
+})
+
+var reset=function(){
+    pcScore=0;
+    userScore=0;
+    updateScore();
+    resetbtnEl.style.boxShadow="0"
+}
+
+resetbtnEl.addEventListener("click",reset)
