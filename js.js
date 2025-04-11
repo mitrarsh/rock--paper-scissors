3a0de7cf35daaba46d51afc58bee13aa01d05cfb
mitrarsh <mitraroshanfekr05@gmail.com>
2025-04-11 18:36:42 +0330

    html css

---
+++ js.js
@@ -0,0 +1,7 @@
+var rockEl = document.getElementById("rock");
+var paperEl = document.getElementById("paper");
+var scissorsEl = document.getElementById("scissors");
+
+var userChoose= function(event){
+    
+}
